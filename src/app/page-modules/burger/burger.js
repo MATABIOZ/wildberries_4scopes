fd893bd87226be7815dd152f92d6a860d4d0190a
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2023-05-19 11:39:51 +0300

    added submenu

--- src/app/page-modules/burger/burger.js
+++ src/app/page-modules/burger/burger.js
@@ -1,12 +1,12 @@
-const burgerBtn = document.querySelector('.burger');
-const sidebar = document.querySelector('.sidebar');
-const closeBtn = document.querySelector('.btn-close');
-const body = document.getElementById('body');
+const burgerBtn = document.querySelector('.burger')
+const sidebar = document.querySelector('.sidebar')
+const closeBtn = document.querySelector('.btn-close')
+const body = document.getElementById('body')
 
 function setBodyStyles(position, width, overflowY) {
-    body.style.position = position;
-    body.style.width = width;
-    body.style.overflowY = overflowY;
+    body.style.position = position
+    body.style.width = width
+    body.style.overflowY = overflowY
 }
 
 burgerBtn.addEventListener('click', function () {
@@ -18,14 +18,14 @@ burgerBtn.addEventListener('click', function () {
 
 closeBtn.addEventListener('click', function () {
     if (sidebar.classList.contains('sidebar_active')) {
-        sidebar.classList.remove('sidebar_active');
+        sidebar.classList.remove('sidebar_active')
         setBodyStyles('', '', '')
     }
 });
 
 document.addEventListener('click', function (event) {
     if (!sidebar.contains(event.target) && !burgerBtn.contains(event.target)) {
-        sidebar.classList.remove('sidebar_active');
+        sidebar.classList.remove('sidebar_active')
         setBodyStyles('', '', '')
     }
 });
