f151e8b2ee80a7fb370fdbe0752a348ab1c65675
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2023-05-23 15:49:03 +0300

    added registration form

--- src/app/page-modules/registration/submenu.js
+++ src/app/page-modules/registration/submenu.js
@@ -1,13 +1,17 @@
+import { registrationWrapperAddClassActive } from './registration.js';
+
 const userBtn = document.querySelector('.user-btn')
 const submenu = document.querySelector('.user__submenu')
 const submenuBtnClose = document.querySelector('.user__submenu-btn-close')
+const btnRegistration = document.querySelector('.user__submenu-btn-registration')
 
-userBtn.addEventListener('click', function() {
-        submenu.classList.toggle('user__submenu_active')
-        checkSubmenuClass()
+userBtn.addEventListener('click', function () {
+    submenu.classList.toggle('user__submenu_active')
+    checkSubmenuClass()
 })
 
 function checkSubmenuClass() {
+
     if (submenu.classList.contains('user__submenu_active')) {
         userBtn.classList.add('user-btn_active')
         document.addEventListener('click', removeSubmenuClassList)
@@ -15,16 +19,25 @@ function checkSubmenuClass() {
         userBtn.classList.remove('user-btn_active')
         document.removeEventListener('click', removeSubmenuClassList);
     }
+
 }
 
 function removeSubmenuClassList(event) {
-    if(!submenu.contains(event.target) && !userBtn.contains(event.target)) {
+
+    if (!submenu.contains(event.target) && !userBtn.contains(event.target)) {
         submenu.classList.remove('user__submenu_active')
-        checkSubmenuClass() 
+        checkSubmenuClass()
     }
+
 }
 
 submenuBtnClose.addEventListener('click', function () {
-        submenu.classList.remove('user__submenu_active')
-        checkSubmenuClass()
+    submenu.classList.remove('user__submenu_active')
+    checkSubmenuClass()
 });
+
+btnRegistration.addEventListener('click', function () {
+    submenu.classList.remove('user__submenu_active')
+    checkSubmenuClass()
+    registrationWrapperAddClassActive()
+})
