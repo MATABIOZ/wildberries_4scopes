e68ca3f83fde638a8eafdfdc4b2fef37bd9f1d8a
Galkevich-Natalia <galkevich.natalia@gmail.com>
2023-05-25 12:25:57 +0300

    Added basket & hover for btn-close

---
+++ src/app/page-modules/basket/basket.js
@@ -0,0 +1,38 @@
+
+const basketBtn = document.querySelector('.basket-btn')
+const basketModal = document.querySelector('.basket__modal')
+const basketModalBtnClose = document.querySelector('.basket__modal-btn-close')
+
+
+basketBtn.addEventListener('click', function () {
+    basketModal.classList.toggle('basket__modal_active')
+    checkBasketModalClass()
+})
+
+function checkBasketModalClass() {
+
+    if (basketModal.classList.contains('basket__modal_active')) {
+        basketBtn.classList.add('basket-btn_active')
+        document.addEventListener('click', removeBasketModalClassList)
+    } else {
+        basketBtn.classList.remove('basket-btn_active')
+        document.removeEventListener('click', removeBasketModalClassList);
+    }
+
+}
+
+function removeBasketModalClassList(event) {
+
+    if (!basketModal.contains(event.target) && !basketBtn.contains(event.target)) {
+        basketModal.classList.remove('basket__modal_active')
+        checkBasketModalClass()
+
+        //console.log(event.target)
+    }
+
+}
+
+basketModalBtnClose.addEventListener('click', function () {
+    basketModal.classList.remove('basket__modal_active')
+    checkBasketModalClass()
+});
