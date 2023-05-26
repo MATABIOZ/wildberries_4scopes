68dfe777cf6f38990edda763d6543782ed1aefc7
VladT8 <tishuk.vladzislau@gmail.com>
2023-05-26 23:57:47 +0300

    add slider swiperjs

--- src/app/page-modules/slider/slider.js
+++ src/app/page-modules/slider/slider.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-   $('.slider__inner').slick({
-      dots: true,
-      speed: 1200,
-      adaptiveHeight: false,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      prevArrow: '<button type="button" class="slick-prev"><img src="./src/app/page-modules/slider/arrow_left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="./src/app/page-modules/slider/arrow_right.svg"></button>'
-   });
-});
+var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 1,
+   spaceBetween: 30,
+   loop: true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+ });
