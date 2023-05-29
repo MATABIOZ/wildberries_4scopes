f8dc2c4bc44d1d336dbb3d0b9117363923d885c9
MATABIOZ <6387125@gmail.com>
2023-05-29 20:04:14 +0300

    added cards

---
+++ src/app/page-modules/product-card/product-card.js
@@ -0,0 +1,186 @@
+// import { getCards } from "../../core/API/cardsApi.js"
+
+const cardsBox = document.querySelector(".cards__inner")
+
+let alyaStore = []
+
+function getCards() {
+	fetch("https://646f451109ff19b12086e48a.mockapi.io/wildberries/cards")
+		.then((response) => {
+			if (response.status === 200) {
+				return response.json()
+			} else {
+				throw Error(response)
+			}
+		})
+		.then((arrayElements) => {
+			alyaStore = arrayElements
+			addCards(arrayElements)
+		})
+		.catch((error) => {
+			alert(error)
+		})
+}
+
+function addCards(arrayElements) {
+	for (let element of arrayElements) {
+		cardsBox.appendChild(createCardElement(element))
+	}
+}
+
+function createCardElement(element) {
+	const item = document.createElement("div")
+	item.classList.add("card")
+
+	item.innerHTML = `
+	<div class="card__view">
+		<img src="${element.image}" alt="image" class="card__img">
+		<button type="button" class="card__btn-quick-view" data-id="${element.id}">Быстрый просмотр</button>
+	</div>
+	<div class="card__inner">
+		<div class="card__info">
+			<h4 class="card__title">${element.title}</h4>
+			<h5 class="card__price">${element.price + "$"}</h5>
+		</div>
+		<div class="card__subinfo">
+			<span class="card__discount">${"-" + element.sale + "%"}</span>
+			<span class="card__old-price">${element.old_price + "$"}</span>
+		</div>
+	</div>
+	<button type="button" class="card__btn-add-basket" data-id="${element.id}">В корзину</button>`
+
+	return item
+}
+
+	// <button type="button" class="card__btn-del-basket" data-id="${element.id}">Удалить</button>
+	// <button type="button" class="card__btn-del-all-basket" data-id="${element.id}">Удалить все</button>
+
+function createCardModal(element) {
+	const item = document.createElement("div")
+	item.classList.add("card-modal")
+
+	item.innerHTML = `
+		<div class="container">
+			<div class="card-modal__wrapper">
+				<img class="card-modal__img" src="${element.image}">
+				<div class="card-modal__inner">
+					<div class="modal-header card-modal__header">
+						<h2 class="modal-header__title">${element.title}</h2>
+						<button class="btn-close" type="button"></button>
+					</div>
+					<p class="card-modal__info"><span>Состав:</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus veniam ipsam nostrum nisi architecto similique praesentium culpa nam illum modi.</p>
+					<div class="card-modal__subinfo">
+						<h3 class="card-modal__price">${element.price + "$"}</h3>
+						<h4 class="card-modal__old-price">${element.old_price + "$"}</h4>
+					</div>
+					<button type="button" class="card-modal__btn-add-basket" data-id="${element.id}">Добавить в корзину</button>
+				</div>
+			</div>
+		</div>`;
+
+	return item
+}
+
+function quickViewClick(event) {
+	if (event.target.classList.contains("card__btn-quick-view")) {
+		const cardId = event.target.dataset.id
+		const selectedCard = alyaStore.find((card) => card.id === cardId)
+		if (selectedCard) {
+			const clonedCard = createCardModal(selectedCard)
+			cardsBox.appendChild(clonedCard)
+		}
+	}
+}
+
+function removeCardModal (event) {
+	if (event.target.classList.contains("btn-close")) {
+		const cardModal = event.target.closest(".card-modal");
+		cardModal.remove();
+	}
+}
+
+function addToBasketClick(event) {
+	if (event.target.classList.contains("card__btn-add-basket") || event.target.classList.contains("card-modal__btn-add-basket")) {
+		const cardId = event.target.dataset.id
+		const selectedCard = alyaStore.find((card) => card.id === cardId)
+		if (selectedCard) {
+			let basketStore = localStorage.getItem("basketStore")
+			let basketArray = []
+
+			if (basketStore) {
+				basketArray = JSON.parse(basketStore)
+			}
+
+			basketArray.push(selectedCard)
+
+			localStorage.setItem("basketStore", JSON.stringify(basketArray))
+		}
+	}
+}
+
+function removeFromBasket(cardId) {
+	let basketStore = localStorage.getItem("basketStore")
+	if (basketStore) {
+		let basketArray = JSON.parse(basketStore)
+		const cardIndex = basketArray.findIndex((card) => card.id === cardId)
+		if (cardIndex !== -1) {
+			basketArray.splice(cardIndex, 1)
+			localStorage.setItem("basketStore", JSON.stringify(basketArray))
+		}
+	}
+}
+
+function removeFromBasketClick(event) {
+	if (event.target.classList.contains("card__btn-del-basket")) {
+		const cardId = event.target.dataset.id
+		removeFromBasket(cardId)
+	}
+}
+
+function removeAllFromBasketClick(event) {
+	if (event.target.classList.contains("card__btn-del-all-basket")) {
+		localStorage.removeItem("basketStore")
+	}
+}
+
+let isFilterActive = false
+
+function filterCardsByTitle(title) {
+	const cardTitles = document.querySelectorAll(".card__title")
+	for (let i = 0; i < cardTitles.length; i++) {
+		const card = cardTitles[i].closest(".card")
+		if (cardTitles[i].textContent === title) {
+			card.style.display = "flex"
+		} else {
+			card.style.display = "none"
+		}
+	}
+}
+
+function resetCardFilter() {
+	const cards = document.querySelectorAll(".card")
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].style.display = "flex"
+	}
+}
+
+function cardTitleClick(event) {
+	if (event.target.classList.contains("card__title")) {
+		if (isFilterActive) {
+			resetCardFilter()
+			isFilterActive = false
+		} else {
+			const title = event.target.textContent
+			filterCardsByTitle(title)
+			isFilterActive = true
+		}
+	}
+}
+
+window.addEventListener("DOMContentLoaded", getCards)
+cardsBox.addEventListener("click", quickViewClick)
+cardsBox.addEventListener("click", addToBasketClick)
+cardsBox.addEventListener("click", removeFromBasketClick)
+cardsBox.addEventListener("click", cardTitleClick)
+cardsBox.addEventListener("click", removeAllFromBasketClick)
+cardsBox.addEventListener("click", removeCardModal)
