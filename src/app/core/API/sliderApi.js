ecffa24addf6bf5812622b6c07624feb50fcaed5
AnnMurr <annmurina1998@mail.ru>
2023-06-05 21:03:31 +0300

    fix slider js

--- src/app/core/API/sliderApi.js
+++ src/app/core/API/sliderApi.js
@@ -1,14 +1,9 @@
-import { createImage } from "../../page-modules/slider/slider";
-
 class SliderApi {
     static imagesUrlApi = 'https://646e07219c677e23218ae1e2.mockapi.io/users/images'
 
     static async getImages() {
-        fetch(this.imagesUrlApi)
+      return await fetch(this.imagesUrlApi)
             .then(response => response.json())
-            .then(data => data.forEach((el, index) => {
-                createImage(el.url, index)
-            }))
             .catch(error => console.error(`Произошла ошибка при загрузке изображения: ${error}`))
     }
 }
